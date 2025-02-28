9952b310299fe3f7632971e5b05a34ab9fb3055e
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-28 13:39:00 +0100

    add a function to genete the markup

--- main.js
+++ main.js
@@ -7,6 +7,31 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
 .then(data => {
     console.log(data);
 
-    
+    const dataList = data;    
+
+
+dataList.forEach(data => {
+
+
+cardEl.insertAdjacentHTML('beforeend', generateMarkup(data))
 })
-.catch(error => console.error(error));
+
+})
+
+function generateMarkup(data){
+    return `<div class="col mb-3 d-flex justify-content-center">
+                        <div class="card-frame p-3">
+                            <div class="pin">
+                                <img id ='img' src="./assets/img/pin.svg" alt="" style="width: 30px;">
+                            </div>
+                            <div class="card-img mb-3">
+                                <img src="${data.url}" alt="">
+                            </div>
+
+                            <div class="card-details">
+                                <div class="date">${data.date}</div>
+                                <div class="card-title fs-5 title">${data.title}</div>
+                            </div>
+                        </div>
+                    </div>`
+}
