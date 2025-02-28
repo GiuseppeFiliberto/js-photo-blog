3eec089ed234af4ce86432d16d427ae34d83c4aa
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-28 12:58:41 +0100

    add fetch but not working

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 
 const cardEl = document.getElementById('card')
+// const dataUrlEndpoint = 'https://lanciweb.github.io/demo/api/pictures/'; 
 
+fetch('https://lanciweb.github.io/demo/api/pictures/')
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+
+    
+})
+.catch(error => console.error(error));
